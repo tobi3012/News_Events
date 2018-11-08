0dadd6cd606c9e994edf96eded5d3714dfaec138
tobi3012 <tranquanghiep3012@gmail.com>
2018-11-08 09:48:54 +0700

    hieptq commit

--- app/src/main/java/com/example/tranq/newsevents/utils/Const.java
+++ app/src/main/java/com/example/tranq/newsevents/utils/Const.java
@@ -8,7 +8,7 @@ public class Const {
 
     public static final String TAB_HOME = "HOME";
     public static final String TAB_NEAR = "NEAR";
-    public static final String TAB_BROWSE = "BROWSE";
+    public static final String TAB_BROWSE = "BROWSE_SEARCH";
     public static final String TAB_MY_PAGE = "MY PAGE";
 
     public static final String EMAIL_REGEX = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
