a98035f850f243d11501a4d542d6efc1a96e3d62
nguyenducdung <nguyenducdung1803@gmail.com>
2018-11-09 17:08:20 +0700

    Code TabNearFragment and fragment_tab_near and add google_maps_api

--- app/src/main/java/com/example/tranq/newsevents/view/fragment/TabNearFragment.java
+++ app/src/main/java/com/example/tranq/newsevents/view/fragment/TabNearFragment.java
@@ -1,33 +1,65 @@
 package com.example.tranq.newsevents.view.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.annotation.Nullable;
 
 import com.example.tranq.newsevents.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * Created by HiepTQ on 11/7/2018.
- */
 
-public class TabNearFragment extends Fragment {
+public class TabNearFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mGoogleMap;
+    private MapView mMapView;
+    private View mView;
 
     public TabNearFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_near, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_tab_near, container, false);
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = mView.findViewById(R.id.map_fragment);
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(139.754681, 35.690511)).title("Staa").snippet("I hope"));
+        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(139.754681, 35.690511)).zoom(16).bearing(0).tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
     }
 
 }
